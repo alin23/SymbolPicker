51777341afa03665a064a6a88f982a2b5d99c2af
Alin Panaitiu <alin.panaitiu@gmail.com>
2025-01-11 20:12:18 +0200

    Categories and dynamic symbols

--- Sources/SymbolPicker/SymbolPicker.swift
+++ Sources/SymbolPicker/SymbolPicker.swift
@@ -9,85 +9,6 @@ import SwiftUI
 
 /// A simple and cross-platform SFSymbol picker for SwiftUI.
 public struct SymbolPicker: View {
-
-    // MARK: - Static constants
-
-    private static var gridDimension: CGFloat {
-        #if os(iOS)
-        return 64
-        #elseif os(tvOS)
-        return 128
-        #elseif os(macOS)
-        return 48
-        #else
-        return 48
-        #endif
-    }
-
-    private static var symbolSize: CGFloat {
-        #if os(iOS)
-        return 24
-        #elseif os(tvOS)
-        return 48
-        #elseif os(macOS)
-        return 24
-        #else
-        return 24
-        #endif
-    }
-
-    private static var symbolCornerRadius: CGFloat {
-        #if os(iOS)
-        return 8
-        #elseif os(tvOS)
-        return 12
-        #elseif os(macOS)
-        return 8
-        #else
-        return 8
-        #endif
-    }
-
-    private static var unselectedItemBackgroundColor: Color {
-        #if os(iOS)
-        return Color(UIColor.systemBackground)
-        #else
-        return .clear
-        #endif
-    }
-
-    private static var selectedItemBackgroundColor: Color {
-        #if os(tvOS)
-        return Color.gray.opacity(0.3)
-        #else
-        return Color.accentColor
-        #endif
-    }
-
-    private static var backgroundColor: Color {
-        #if os(iOS)
-        return Color(UIColor.secondarySystemBackground)
-        #else
-        return .clear
-        #endif
-    }
-
-    private static var deleteButtonTextVerticalPadding: CGFloat {
-        #if os(iOS)
-        return 12.0
-        #else
-        return 8.0
-        #endif
-    }
-
-    // MARK: - Properties
-    
-    @Binding public var symbol: String?
-    @State private var searchText = ""
-    @Environment(\.dismiss) private var dismiss
-
-    private let nullable: Bool
-
     // MARK: - Init
 
     /// Initializes `SymbolPicker` with a string binding to the selected symbol name.
@@ -98,14 +19,15 @@ public struct SymbolPicker: View {
     public init(symbol: Binding<String>) {
         self.init(
             symbol: Binding {
-                return symbol.wrappedValue
+                symbol.wrappedValue
             } set: { newValue in
                 /// As the `nullable` is set to `false`, this can not be `nil`
                 if let newValue {
                     symbol.wrappedValue = newValue
                 }
             },
-            nullable: false)
+            nullable: false
+        )
     }
 
     /// Initializes `SymbolPicker` with a nullable string binding to the selected symbol name.
@@ -119,10 +41,147 @@ public struct SymbolPicker: View {
     }
 
     /// Private designated initializer.
-    private init(symbol: Binding<String?>,
-                 nullable: Bool) {
-        self._symbol = symbol
+    private init(
+        symbol: Binding<String?>,
+        nullable: Bool
+    ) {
+        _symbol = symbol
         self.nullable = nullable
+    }
+
+    @Binding public var symbol: String?
+
+    public var body: some View {
+        #if !os(macOS)
+            NavigationView {
+                ZStack {
+                    #if os(iOS)
+                        Self.backgroundColor.edgesIgnoringSafeArea(.all)
+                    #endif
+                    searchableSymbolGrid
+
+                    #if os(iOS) || os(visionOS)
+                        if canDeleteIcon {
+                            VStack {
+                                Spacer()
+
+                                deleteButton
+                                    .padding()
+                                    .background(.regularMaterial)
+                            }
+                        }
+                    #endif
+                }
+                #if os(iOS)
+                .navigationBarTitleDisplayMode(.inline)
+                #endif
+                #if !os(tvOS)
+                /// tvOS can use back button on remote
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Text(LocalizedString("cancel"))
+                        }
+                    }
+                }
+                #endif
+            }
+            .navigationViewStyle(.stack)
+        #else
+            searchableSymbolGrid
+                .frame(width: 660, height: 400, alignment: .center)
+                .background(.regularMaterial)
+        #endif
+    }
+
+    @State var selectedCategories: Set<String> = ["cameraandphotos"]
+
+    // MARK: - Static constants
+
+    private static var gridDimension: CGFloat {
+        #if os(iOS)
+            return 64
+        #elseif os(tvOS)
+            return 128
+        #elseif os(macOS)
+            return 80
+        #else
+            return 48
+        #endif
+    }
+
+    private static var symbolSize: CGFloat {
+        #if os(iOS)
+            return 24
+        #elseif os(tvOS)
+            return 48
+        #elseif os(macOS)
+            return 32
+        #else
+            return 24
+        #endif
+    }
+
+    private static var symbolCornerRadius: CGFloat {
+        #if os(iOS)
+            return 8
+        #elseif os(tvOS)
+            return 12
+        #elseif os(macOS)
+            return 8
+        #else
+            return 8
+        #endif
+    }
+
+    private static var unselectedItemBackgroundColor: Color {
+        #if os(iOS)
+            return Color(UIColor.systemBackground)
+        #else
+            return .clear
+        #endif
+    }
+
+    private static var selectedItemBackgroundColor: Color {
+        #if os(tvOS)
+            return Color.gray.opacity(0.3)
+        #else
+            return Color.accentColor
+        #endif
+    }
+
+    private static var backgroundColor: Color {
+        #if os(iOS)
+            return Color(UIColor.secondarySystemBackground)
+        #else
+            return .clear
+        #endif
+    }
+
+    private static var deleteButtonTextVerticalPadding: CGFloat {
+        #if os(iOS)
+            return 12.0
+        #else
+            return 8.0
+        #endif
+    }
+
+    @State private var searchText = ""
+    @Environment(\.dismiss) private var dismiss
+
+    private let nullable: Bool
+
+    private var canDeleteIcon: Bool {
+        nullable && symbol != nil
+    }
+
+    private var symbols: [String] {
+        guard !selectedCategories.isEmpty, selectedCategories != ["all"], !Symbols.shared.symbolsByCategory.isEmpty else {
+            return Symbols.shared.symbols
+        }
+        return selectedCategories.flatMap { Symbols.shared.symbolsByCategory[$0] ?? [] }
     }
 
     // MARK: - View Components
@@ -130,112 +189,143 @@ public struct SymbolPicker: View {
     @ViewBuilder
     private var searchableSymbolGrid: some View {
         #if os(iOS)
-        symbolGrid
-            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
-        #elseif os(tvOS)
-        VStack {
-            TextField(LocalizedString("search_placeholder"), text: $searchText)
-                .padding(.horizontal, 8)
-                .autocapitalization(.none)
-                .disableAutocorrection(true)
             symbolGrid
-        }
+                .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+        #elseif os(tvOS)
+            VStack {
+                TextField(LocalizedString("search_placeholder"), text: $searchText)
+                    .padding(.horizontal, 8)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                symbolGrid
+            }
 
         /// `searchable` is crashing on tvOS 16. What the hell aPPLE?
         ///
         /// symbolGrid
         ///     .searchable(text: $searchText, placement: .automatic)
         #elseif os(macOS)
-        VStack(spacing: 0) {
-            HStack {
-                TextField(LocalizedString("search_placeholder"), text: $searchText)
-                    .textFieldStyle(.plain)
-                    .font(.system(size: 18.0))
-                    .disableAutocorrection(true)
-
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "xmark.circle.fill")
-                        .resizable()
-                        .frame(width: 16.0, height: 16.0)
-                }
-                .buttonStyle(.borderless)
-            }
-            .padding()
-
-            Divider()
-
-            symbolGrid
-
-            if canDeleteIcon {
-                Divider()
+            VStack(spacing: 0) {
                 HStack {
-                    Spacer()
-                    deleteButton
-                        .padding(.horizontal)
-                        .padding(.vertical, 8.0)
+                    TextField(LocalizedString("search_placeholder"), text: $searchText)
+                        .textFieldStyle(.plain)
+                        .font(.system(size: 18.0))
+                        .disableAutocorrection(true)
+
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .resizable()
+                            .frame(width: 16.0, height: 16.0)
+                    }
+                    .buttonStyle(.borderless)
+                }
+                .padding()
+
+                Divider()
+
+                HStack {
+                    if Symbols.shared.categories.count > 0 {
+                        categoriesList
+                    }
+                    symbolGrid
+                }
+
+                if canDeleteIcon {
+                    Divider()
+                    HStack {
+                        Spacer()
+                        deleteButton
+                            .padding(.horizontal)
+                            .padding(.vertical, 8.0)
+                    }
                 }
             }
-        }
         #else
-        symbolGrid
-            .searchable(text: $searchText, placement: .automatic)
+            symbolGrid
+                .searchable(text: $searchText, placement: .automatic)
         #endif
     }
 
+    private var categoriesList: some View {
+        List(selection: $selectedCategories) {
+            ForEach(Symbols.shared.categories) { category in
+                Label(category.id, systemImage: category.icon)
+                    .tag(category.id)
+            }
+        }
+        .frame(width: 190)
+        .listStyle(.sidebar)
+    }
     private var symbolGrid: some View {
         ScrollView {
             #if os(tvOS) || os(watchOS)
-            if canDeleteIcon {
-                deleteButton
-            }
+                if canDeleteIcon {
+                    deleteButton
+                }
             #endif
 
             LazyVGrid(columns: [GridItem(.adaptive(minimum: Self.gridDimension, maximum: Self.gridDimension))]) {
-                ForEach(symbols.filter { searchText.isEmpty ? true : $0.localizedCaseInsensitiveContains(searchText) }, id: \.self) { thisSymbol in
+                let searchWords = searchText.split(separator: " ").map { String($0).split(separator: ".").map { String($0) } }.joined()
+                let symbols = searchWords.isEmpty ? symbols : symbols.filter { symbol in
+                    searchWords.allSatisfy { searchWord in
+                        symbol.contains(searchWord)
+                    }
+                }
+                ForEach(symbols, id: \.self) { thisSymbol in
                     Button {
                         symbol = thisSymbol
                         dismiss()
                     } label: {
-                        if thisSymbol == symbol {
-                            Image(systemName: thisSymbol)
-                                .font(.system(size: Self.symbolSize))
+                        VStack(spacing: 1) {
+                            if thisSymbol == symbol {
+                                Image(systemName: thisSymbol)
+                                    .font(.system(size: Self.symbolSize))
                                 #if os(tvOS)
-                                .frame(minWidth: Self.gridDimension, minHeight: Self.gridDimension)
+                                    .frame(minWidth: Self.gridDimension, minHeight: Self.gridDimension)
                                 #else
-                                .frame(maxWidth: .infinity, minHeight: Self.gridDimension)
+                                    .frame(maxWidth: .infinity, minHeight: Self.gridDimension)
                                 #endif
-                                .background(Self.selectedItemBackgroundColor)
+                                    .background(Self.selectedItemBackgroundColor)
                                 #if os(visionOS)
-                                .clipShape(Circle())
+                                    .clipShape(Circle())
                                 #else
-                                .cornerRadius(Self.symbolCornerRadius)
+                                    .cornerRadius(Self.symbolCornerRadius)
                                 #endif
-                                .foregroundColor(.white)
-                        } else {
-                            Image(systemName: thisSymbol)
-                                .font(.system(size: Self.symbolSize))
-                                .frame(maxWidth: .infinity, minHeight: Self.gridDimension)
-                                .background(Self.unselectedItemBackgroundColor)
-                                .cornerRadius(Self.symbolCornerRadius)
-                                .foregroundColor(.primary)
+                                    .foregroundColor(.white)
+                            } else {
+                                Image(systemName: thisSymbol)
+                                    .font(.system(size: Self.symbolSize))
+                                    .frame(maxWidth: .infinity, minHeight: Self.gridDimension)
+                                    .background(Self.unselectedItemBackgroundColor)
+                                    .cornerRadius(Self.symbolCornerRadius)
+                                    .foregroundColor(.primary)
+                            }
+                            Text(thisSymbol)
+                                .font(.system(size: 8))
+                                .lineLimit(2, reservesSpace: true)
+                                .multilineTextAlignment(.center)
+                                .allowsTightening(true)
+                                .frame(maxWidth: Self.gridDimension)
+                                .fixedSize()
                         }
+                        .help(thisSymbol)
                     }
                     .buttonStyle(.plain)
                     #if os(iOS)
-                    .hoverEffect(.lift)
+                        .hoverEffect(.lift)
                     #endif
                 }
             }
-            .padding(.horizontal)
+            .padding(.horizontal, 4)
 
             #if os(iOS) || os(visionOS)
-            /// Avoid last row being hidden.
-            if canDeleteIcon {
-                Spacer()
-                    .frame(height: Self.gridDimension * 2)
-            }
+                /// Avoid last row being hidden.
+                if canDeleteIcon {
+                    Spacer()
+                        .frame(height: Self.gridDimension * 2)
+                }
             #endif
         }
     }
@@ -246,68 +336,15 @@ public struct SymbolPicker: View {
             dismiss()
         } label: {
             Label(LocalizedString("remove_symbol"), systemImage: "trash")
-                #if !os(tvOS) && !os(macOS)
+            #if !os(tvOS) && !os(macOS)
                 .frame(maxWidth: .infinity)
-                #endif
-                #if !os(watchOS)
-                .padding(.vertical, Self.deleteButtonTextVerticalPadding)
-                #endif
-                .background(Self.unselectedItemBackgroundColor)
-                .clipShape(RoundedRectangle(cornerRadius: 12.0, style: .continuous))
-        }
-    }
-
-    public var body: some View {
-        #if !os(macOS)
-        NavigationView {
-            ZStack {
-                #if os(iOS)
-                Self.backgroundColor.edgesIgnoringSafeArea(.all)
-                #endif
-                searchableSymbolGrid
-
-                #if os(iOS) || os(visionOS)
-                if canDeleteIcon {
-                    VStack {
-                        Spacer()
-
-                        deleteButton
-                            .padding()
-                            .background(.regularMaterial)
-                    }
-                }
-                #endif
-            }
-            #if os(iOS)
-            .navigationBarTitleDisplayMode(.inline)
             #endif
-            #if !os(tvOS)
-            /// tvOS can use back button on remote
-            .toolbar {
-                ToolbarItem(placement: .cancellationAction) {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Text(LocalizedString("cancel"))
-                    }
-                }
-            }
+            #if !os(watchOS)
+            .padding(.vertical, Self.deleteButtonTextVerticalPadding)
             #endif
+            .background(Self.unselectedItemBackgroundColor)
+            .clipShape(RoundedRectangle(cornerRadius: 12.0, style: .continuous))
         }
-        .navigationViewStyle(.stack)
-        #else
-        searchableSymbolGrid
-            .frame(width: 540, height: 340, alignment: .center)
-            .background(.regularMaterial)
-        #endif
-    }
-
-    private var canDeleteIcon: Bool {
-        nullable && symbol != nil
-    }
-
-    private var symbols: [String] {
-        Symbols.shared.symbols
     }
 
 }
@@ -319,25 +356,25 @@ private func LocalizedString(_ key: String.LocalizationValue) -> String {
 // MARK: - Debug
 
 #if DEBUG
-#Preview("Normal") {
-    struct Preview: View {
-        @State private var symbol: String? = "square.and.arrow.up"
-        var body: some View {
-            SymbolPicker(symbol: $symbol)
+    #Preview("Normal") {
+        struct Preview: View {
+            @State private var symbol: String? = "square.and.arrow.up"
+            var body: some View {
+                SymbolPicker(symbol: $symbol)
+            }
         }
+        return Preview()
     }
-    return Preview()
-}
 
-#Preview("Filter Example") {
-    Symbols.shared.filter = { $0.contains(".circle") }
-    
-    struct Preview: View {
-        @State private var symbol: String? = "square.and.arrow.up.circle.fill"
-        var body: some View {
-            SymbolPicker(symbol: $symbol)
+    #Preview("Filter Example") {
+        Symbols.shared.filter = { $0.contains(".circle") }
+
+        struct Preview: View {
+            @State private var symbol: String? = "square.and.arrow.up.circle.fill"
+            var body: some View {
+                SymbolPicker(symbol: $symbol)
+            }
         }
+        return Preview()
     }
-    return Preview()
-}
 #endif
--- Sources/SymbolPicker/Symbols.swift
+++ Sources/SymbolPicker/Symbols.swift
@@ -7,12 +7,38 @@
 
 import Foundation
 
+public struct Category: Identifiable {
+    public let id: String
+    public let icon: String
+}
+
 /// Simple singleton class for providing symbols list per platform availability.
 @MainActor
 public class Symbols: Sendable {
+    private init() {
+        let filename = if #available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *) {
+            "sfsymbol6"
+        } else if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, visionOS 1.0, *) {
+            "sfsymbol5"
+        } else {
+            "sfsymbol4"
+        }
+        if #available(macOS 13.0, *), let (symbolsByCategory, allSymbols) = Self.fetchSymbolsFromSystem() ?? Self.fetchSymbolsFromBundle() {
+            self.symbolsByCategory = symbolsByCategory
+            self.allSymbols = allSymbols
+            categories = Self.fetchCategories()
+        } else {
+            symbolsByCategory = [:]
+            categories = []
+            allSymbols = Self.fetchSymbols(fileName: filename)
+        }
+        symbols = allSymbols
+    }
 
     /// Singleton instance.
     public static let shared = Symbols()
+
+    public let categories: [Category]
 
     /// Filter closure that checks each symbol name string should be included.
     public var filter: ((String) -> Bool)? {
@@ -28,26 +54,71 @@ public class Symbols: Sendable {
     /// Array of the symbol name strings to be displayed.
     private(set) var symbols: [String]
 
+    private(set) var symbolsByCategory: [String: [String]]
+
+    private static let UNWANTED_CATEGORIES = ["whatsnew", "variablecolor", "multicolor"]
+
     /// Array of all available symbol name strings.
     private let allSymbols: [String]
 
-    private init() {
-        let filename = if #available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *) {
-            "sfsymbol6"
-        } else if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, visionOS 1.0, *) {
-            "sfsymbol5"
-        } else {
-            "sfsymbol4"
+    @available(macOS 13.0, *)
+    private static func fetchSymbols(plist: NSDictionary) -> ([String: [String]], [String])? {
+        var symbolsByCategory = [String: [String]]()
+        var allSymbols = [String]()
+        let symbols = (plist as? [String: [String]]) ?? [:]
+
+        for (symbol, categories) in symbols {
+            for category in categories {
+                if symbolsByCategory[category] == nil {
+                    symbolsByCategory[category] = [String]()
+                }
+                symbolsByCategory[category]!.append(symbol)
+            }
+            allSymbols.append(symbol)
         }
-        self.allSymbols = Self.fetchSymbols(fileName: filename)
-        self.symbols = self.allSymbols
+
+        return (symbolsByCategory.mapValues { $0.sorted() }, allSymbols.sorted())
+    }
+    private static func fetchCategories() -> [Category] {
+        let bundle = Bundle(path: "/System/Library/PrivateFrameworks/SFSymbols.framework/Versions/A/Resources/CoreGlyphs.bundle") ?? Bundle.module
+        guard let resourcePath = bundle.path(forResource: "categories", ofType: "plist"),
+              let plist = NSArray(contentsOfFile: resourcePath)
+        else {
+            return []
+        }
+
+        let cats = (plist as? [[String: String]]) ?? []
+        return cats
+            .filter { !UNWANTED_CATEGORIES.contains($0["key"] ?? "") }
+            .map { cat in
+                Category(id: cat["key"] ?? "", icon: cat["icon"] ?? "")
+            }
+    }
+    private static func fetchSymbolsFromBundle() -> ([String: [String]], [String])? {
+        guard let path = Bundle.module.path(forResource: "symbol_categories", ofType: "plist"),
+              let plist = NSDictionary(contentsOfFile: path)
+        else {
+            return nil
+        }
+        return fetchSymbols(plist: plist)
+    }
+
+    private static func fetchSymbolsFromSystem() -> ([String: [String]], [String])? {
+        guard let bundle = Bundle(path: "/System/Library/PrivateFrameworks/SFSymbols.framework/Versions/A/Resources/CoreGlyphs.bundle"),
+              let resourcePath = bundle.path(forResource: "symbol_categories", ofType: "plist"),
+              let plist = NSDictionary(contentsOfFile: resourcePath)
+        else {
+            return nil
+        }
+        return fetchSymbols(plist: plist)
     }
 
     private static func fetchSymbols(fileName: String) -> [String] {
         guard let path = Bundle.module.path(forResource: fileName, ofType: "txt"),
-              let content = try? String(contentsOfFile: path) else {
+              let content = try? String(contentsOfFile: path)
+        else {
             #if DEBUG
-            assertionFailure("[SymbolPicker] Failed to load bundle resource file.")
+                assertionFailure("[SymbolPicker] Failed to load bundle resource file.")
             #endif
             return []
         }
